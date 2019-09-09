d3a9339c047fea9ce43e128ff81b246bdc0ebffc
megan_sin <Megans310>
2019-09-09 15:07:33 -0400

    premerge: sun counter

--- src/Main.java
+++ src/Main.java
@@ -10,31 +10,45 @@ public class Main extends JPanel{
     private Timer timer;
     private ArrayList<Sprite> zombies;
     private ArrayList <Sprite> plants;
-
+    private int j,x,sunTimer, sunTot;
 
 
     public Main(){
 
         plants = new ArrayList<Sprite>();
 
+        sunTot = 50;
+
         timer = new Timer(1000 / 60, e -> update());
         timer.start();
         setKeyListener();
         mouseListener();
 
-
-
+        j = 0;
         zombies = new ArrayList<Sprite>();
 
-        zombies.add(new RegZ());
-        update();
+
+        while(j<5){
+            x = j*100;
+            zombies.add(new RegZ(x));
+
+            j += 1;
+        }
+
+
 
 
     }
 
     public void update() {
+        //60 per second
+        sunTimer ++;
 
-//hi
+
+        if(sunTimer % 300 ==0){
+            sunTot += 25;
+        }
+
         repaint();
     }
 
@@ -46,6 +60,7 @@ public class Main extends JPanel{
         for(Sprite z: zombies){
 
             z.draw(g2);
+            z.update();
 
         }
 
@@ -53,6 +68,10 @@ public class Main extends JPanel{
             plant.draw(g2);
         }
 
+        g2.setColor(Color.BLACK);
+        Font f = new Font(Font.SANS_SERIF, Font.BOLD, 20);
+        g2.setFont(f);
+        g2.drawString("Sun" + " " + sunTot, 20, 20);
 
 
 
@@ -110,6 +129,7 @@ public class Main extends JPanel{
             }
         });
     }
+
 
 
     public static void main(String[] args) {
--- src/RegZ.java
+++ src/RegZ.java
@@ -1,15 +1,24 @@
+import javax.swing.*;
+import java.net.URL;
+import java.util.WeakHashMap;
+
 public class RegZ extends Sprite {
 
 //    private int level;
 
-    public RegZ() {
-        super(100, 100, EAST);
-        setPic("regZ.png", WEST);
+    public RegZ(int x) {
+        super(1200, 100, WEST);
+        setPic("regZ.png",WEST);
+
     }
 
     @Override
     public void update() {
-        setSpeed(3);
+        setSpeed(1);
+        super.update();
     }
+
+
+
 
 }
