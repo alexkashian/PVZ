22857ddc31bd07289915d56fc212b17057754001
alex_kashian <alex_kashian@student.wayland.k12.ma.us>
2019-09-11 11:26:03 -0400

    re-naming plant png's

--- src/PeaShooter.java
+++ src/PeaShooter.java
@@ -3,7 +3,7 @@ public class PeaShooter extends Sprite{
 
     public PeaShooter(int r, int c, int direction) {
         super((c*110)+15, (r*125)+20, direction);
-        setPic("P.png", EAST);
+        setPic("peashooter.png", EAST);
         setSpeed(0); //GUESS?!
 //force commit
 
--- src/Sunflower.java
+++ src/Sunflower.java
@@ -1,7 +1,7 @@
 public class Sunflower extends Sprite{
     public Sunflower(int r, int c, int direction) {
         super((c*110)+15, (r*125)+20, direction);
-        setPic("sun.png", EAST);
+        setPic("sunflower.png", EAST);
         setSpeed(0); //GUESS?!
     }
     //force commit
